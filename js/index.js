26c981c0c4db28ff38e6ff7c4d72a1770888bccb
Yelous25 <105781548+yelous25@users.noreply.github.com>
2023-09-12 00:42:47 +0000

    done

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+export const increment = (element) => {
+    document.querySelector(element).value++
+}
+
+export const decrement = (element) => {
+    document.querySelector(element).value--
+}
+
+
--- main.js
+++ main.js
@@ -1,2 +1,16 @@
+import { increment, decrement } from './js'
 import './styles/index.css'
+
+window.increment = increment
+window.decrement = decrement
+
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    alert('Checkout Validated')
+
+});
+
 
